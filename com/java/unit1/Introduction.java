f4712c3ea95598ac139e374cdfb8c7aa21b70570
pavan-varanasi <pavan.varanasi26@gmail.com>
2020-03-15 11:39:50 +0530

    Added logic

--- com/java/unit1/Introduction.java
+++ com/java/unit1/Introduction.java
@@ -1,7 +1,21 @@
 package com.java.unit1;
 
 public class Introduction {
+	private int first;
+	private int second;
+	private int sum;
+	
+	public Introduction(int x, int y) {
+		this.first = x;
+		this.second = y;
+		
+		sum = x+y;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Introduction to java");
+		System.out.println("Adding two two numbers");
+		Introduction intro = new Introduction(5, 7);
+		
+		System.out.println("The sum is:" + intro.sum);
 	}
 }
